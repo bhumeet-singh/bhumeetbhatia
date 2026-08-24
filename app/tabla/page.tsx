c6de5a536542b5696c28ce1d7beac28c113b09f5
Bhumeet Singh <bhumeetsingh@umass.edu>
2026-08-24 15:21:17 -0400

    Initial Next.js scaffold with 3-page structure

---
+++ app/tabla/page.tsx
@@ -0,0 +1,3 @@
+export default function TablaPage() {
+  return <h1 className="p-10 text-3xl">Tabla Page</h1>;
+}
---
+++ app/work/page.tsx
@@ -0,0 +1,3 @@
+export default function WorkPage() {
+  return <h1 className="p-10 text-3xl">Work Page</h1>;
+}
